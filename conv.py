93eda5c95ae9e550f702acde51c4b82a599cd6d9
Kyeongsup Choi <kyeongsupchoi@gmail.com>
2024-10-13 12:55:19 +0900

    add csvs

--- conv.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Read CSV file
-df = pd.read_csv('tourism.csv', encoding='euc-kr')
-
-
-
-# Write it back to CSV with UTF-8 encoding (or any other changes you want to make)
-df.to_csv('tourist.csv', index=False, encoding='utf-8-sig')
